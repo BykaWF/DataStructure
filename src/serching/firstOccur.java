a75e275ca53b9f19f5c22d23016822078dfe4cb8
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-06 14:51:18 +0200

    firstOccurElement (Recursive)

---
+++ src/serching/firstOccur.java
@@ -0,0 +1,30 @@
+package serching;
+
+public class firstOccur {
+    public static void main(String[] args) {
+        int[] arr = {5,5,5};
+        System.out.println(firstOccurElement(arr,0, arr.length - 1, 5));
+    }
+
+    static int firstOccurElement(int[] arr, int start, int end, int x){
+        if(start > end){
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if(arr[mid] > x){
+            return firstOccurElement(arr,start, end - 1, x);
+        }
+
+        if(arr[mid] < x){
+            return firstOccurElement(arr,mid + 1,end,x);
+        }
+
+        if(mid == 0 || arr[mid - 1] != x){
+            return mid;
+        }else {
+            return firstOccurElement(arr,0, mid - 1, x);
+        }
+    }
+}
