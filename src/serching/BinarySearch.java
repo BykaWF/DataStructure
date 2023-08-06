254afedce2da528de1c5bd461a665c856a2004b6
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-06 12:51:45 +0200

    BinarySearch (Recursive)

--- src/serching/BinarySearch.java
+++ src/serching/BinarySearch.java
@@ -3,7 +3,11 @@ package serching;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {5,15,25};
-        System.out.println(binarySearch(arr,arr.length,25));
+        int start = 0;
+        int end = arr.length - 1;
+        int target = 25;
+
+        System.out.println(binarySearch2(arr,start,end,target));
     }
 
     static int binarySearch(int[] arr, int n, int target){
@@ -27,5 +31,23 @@ public class BinarySearch {
         }
 
         return -1;
+    }
+
+    static int binarySearch2(int[] arr,int start , int end, int x){
+        if(start > end){
+            return -1;
+        }
+
+        int mid = (start + end ) / 2;
+
+        if(arr[mid] == x){
+            return mid;
+        }
+        if(arr[mid] > x){
+            return binarySearch2(arr,start,mid - 1,x);
+        }else {
+            return binarySearch2(arr,mid + 1,end,x);
+        }
+
     }
 }
